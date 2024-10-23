fcdcc2cae19e21e257ac7292de0c3365b51e5b5f
Sunil Kumar Sukesan <sunilkumar.sukesan@gmail.com>
2024-10-24 00:20:34 +0530

    updated the accordions

--- main.js
+++ main.js
@@ -1,3 +1,21 @@
+// Accordion script
+document.addEventListener('DOMContentLoaded', function() {
+  const headers = document.querySelectorAll('.accordion-header');
+
+  headers.forEach(header => {
+    header.addEventListener('click', () => {
+      const content = header.nextElementSibling;
+      content.style.display = content.style.display === 'block' ? 'none' : 'block';
+
+       // Toggle the arrow's rotation
+            this.classList.toggle('active');
+
+             // Check if the active class is being added/removed
+                  console.log('Active classes:', this.classList);
+    });
+  });
+});
+
 // Fetch GitHub repositories and display them
 fetch('https://api.github.com/users/sunilkumarsukesan/repos') // Replace 'your-username' with your actual GitHub username
   .then(response => response.json())
